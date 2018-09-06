520be562bbab3363c2183b5cd934ea84aa3c89f2
nick1658 <hzf1658@yeah.net>
2018-09-06 17:06:02 +0800

    增加测速功能1

--- Apps/main.c
+++ Apps/main.c
@@ -328,7 +328,7 @@ void TaskStart(void *pdata)
 			case 0:{
 				ALL_STOP();//停掉所有的输出
 				if (sys_env.sys_runing_time_total > 0){
-					sys_env.coin_speed = ((processed_coin_info.total_coin - processed_coin_info.total_coin_old) * 60) / (sys_env.sys_runing_time_total / 10000);
+					//sys_env.coin_speed = ((processed_coin_info.total_coin - processed_coin_info.total_coin_old) * 60) / (sys_env.sys_runing_time_total / 10000);
 					//refresh_data ();
 				}
 				reset_active_resister (ACT_L_R_ACCEPTING_COIN, 0);
